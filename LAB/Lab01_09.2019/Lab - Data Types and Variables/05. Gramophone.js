9b71925a3cf80693da37ddb412f9d32376fc09cf
Legolas351 <45262283+Legolas351@users.noreply.github.com>
2019-09-30 10:00:08 +0300

    Delete 05. Gramophone.js

--- LAB/Lab01_09.2019/Lab - Data Types and Variables/05. Gramophone.js
+++
@@ -1,6 +0,0 @@
-function grami(band, album, song) {
-  let time = (album.length * band.length * song.length) / 2;
-  let rotations = Math.ceil(time / 2.5);
-  console.log(`The plate was rotated ${rotations} times.`);
-}
-grami("Black Sabbath", "Paranoid", "War Pigs");
