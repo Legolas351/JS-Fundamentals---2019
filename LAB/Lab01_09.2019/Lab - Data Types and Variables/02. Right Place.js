a186c6f70a8b4521581c268085fe299863e0d9c2
Legolas351 <45262283+Legolas351@users.noreply.github.com>
2019-09-30 09:59:41 +0300

    Delete 02. Right Place.js

--- LAB/Lab01_09.2019/Lab - Data Types and Variables/02. Right Place.js
+++
@@ -1,7 +0,0 @@
-function place(missing, cahr, check) {
-  let replace = missing.replace("_", cahr);
-  let output = replace === check ? "Matched" : "Not Matched";
-
-  console.log(output);
-}
-place("Str_ng", "I", "Strong");
